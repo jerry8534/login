df8f7eeda2be0033747cace9ab9256311b4ea6a9
jerry8534 <jerryjjinu@gmail.com>
2024-09-11 22:59:21 +0900

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from app.config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
